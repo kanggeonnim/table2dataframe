5d46b8fa67512dd6f46b99ed7a4518aa86b0fa4e
kanggeonnim <gun4329@gmail.com>
2024-03-03 18:49:06 +0900

    add files

---
+++ main.py
@@ -0,0 +1,60 @@
+import olefile
+import zlib
+import struct
+
+def get_hwp_text(filename):
+    f = olefile.OleFileIO(filename)
+    dirs = f.listdir()
+
+    # HWP 파일 검증
+    if ["FileHeader"] not in dirs or \
+            ["\x05HwpSummaryInformation"] not in dirs:
+        raise Exception("Not Valid HWP.")
+
+    # 문서 포맷 압축 여부 확인
+    header = f.openstream("FileHeader")
+    header_data = header.read()
+    is_compressed = (header_data[36] & 1) == 1
+
+    # Body Sections 불러오기
+    nums = []
+    for d in dirs:
+        if d[0] == "BodyText":
+            nums.append(int(d[1][len("Section"):]))
+    sections = ["BodyText/Section" + str(x) for x in sorted(nums)]
+
+    # 전체 text 추출
+    text = ""
+    for section in sections:
+        bodytext = f.openstream(section)
+        data = bodytext.read()
+        if is_compressed:
+            unpacked_data = zlib.decompress(data, -15)
+        else:
+            unpacked_data = data
+
+        # 각 Section 내 text 추출
+        section_text = ""
+        i = 0
+        size = len(unpacked_data)
+        while i < size:
+            header = struct.unpack_from("<I", unpacked_data, i)[0]
+            rec_type = header & 0x3ff # header의 마지막 10비트를 추출하는 연산
+            rec_len = (header >> 20) & 0xfff
+
+            if rec_type in [67]:
+                # i + 4에서 4byte는 헤더의 크기
+                rec_data = unpacked_data[i + 4:i + 4 + rec_len]
+                section_text += rec_data.decode('utf-16')
+                section_text += "\n"
+
+            i += 4 + rec_len
+
+        text += section_text
+        text += "\n"
+
+    return text
+
+
+text = get_hwp_text('C:/Users/SSAFY/Downloads/21대 국회의원 공약이행현황/057.220831_더불어민주당 부산북강서갑_전재수 의원실_21대 국회의원 공약이행 및 의정활동 평가.hwp')
+print(text)
